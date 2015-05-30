7d801fd57e431209ae42221f32f1a089e9b2bf2b
Perry <thescratcherman@gmail.com>
2015-05-29 23:08:00 -0400

    Added selection of a specific group from the user's list of groups

--- absentee-finder.py
+++ absentee-finder.py
@@ -5,7 +5,24 @@ import json
 GROUPME_API = "https://api.groupme.com/v3"
 
 def main():
-    pass
+    # gather required background information
+    user_token = input("Please enter your API Token: ")
+    group = select_group(user_token)
+    group_id = group["group_id"]
+
+# lists user's groups, asks for user to select a specific group, returns it.
+def select_group(user_token):
+    # Get list of groups
+    groups = make_request(GROUPME_API, "/groups", user_token)
+    names = [group["name"] for group in groups]
+
+    # print groups for user to select
+    for i in range(len(names)):
+        print("[" + str(i) + "] " + names[i])
+    selected_index = int(input("Please enter the number of your selected group: "))
+
+    # return selected group
+    return groups[selected_index]
 
 # fetches resource at URL, converts JSON response to useful Object
 def make_request(base_url, additional_url, token):
